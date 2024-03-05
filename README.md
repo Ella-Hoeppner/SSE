9299384fb66424ed824c0007565d8937ee2b6825
Ella Hoeppner <ellahoeppner@gmail.com>
2024-03-05 02:35:26 -0500

    initial commit, forking from Quoot

---
+++ README.md
@@ -0,0 +1,37 @@
+# GSE
+
+A parser for **G**eneralized **S**-**E**xpressions.
+
+## Background and Motivation
+
+S-expressions are a simple but powerful text format that come from the Lisp family of programming languages. Each S-expression is either an *atom* or a *list*, where an atom is a simple indivisible token like a word, number, or string, and a list is a sequence of simpler S-expressions enclosed in parentheses and separated by whitespace. The following are all valid S-expressions:
+
+```
+hello
+
+(hello)
+
+(hello goodbye)
+
+(hello goodbye (hello again))
+
+(s-expressions ((can be) (arbitrarily (deeply (nested!)))))
+```
+
+Most Lisps add a few extra syntactic conveniences on top of pure S-expressions. A common example is the quote operator, usually denoted with the `'` character, which can be used as a prefix to an S-expression to change the way the interpreter treats the expression. For instance, `'hello` and `'(hello goodbye)` are valid expressions in most Lisps. Prefix operators like this can be interpreted as simple syntactic sugar over normal S-expressions, e.g. the previous examples could be seen as shorthand for `(quote hello)` and `(quote (hello goodbye))`, respectively.
+
+Clojure, a modern Lisp dialect, diverges even further from a pure S-expression syntax by using several distinct types of delimiters aside from parentheses, with each kind of delimiter corresponding to a different data structure or semantic element. While `(...)` is used to represent lists, `[...]` is used to represent vectors, `{...}` for hashmaps, `#{...}` for sets, and `#(...)` for lambdas. These extra delimiters allow Clojure code to be more expressive and concise than traditional Lisps, while staying close enough to traditional S-expressions to maintain the benefits like [structural editing](https://clojure.org/guides/structural_editing).
+
+This project aims to establish a new text format, called "GSE" for **G**eneralized **S**-**E**xpressions, that extends traditional S-expressions with the capacity to handle arbitrary (unary) prefix operators, as well as arbitrary delimiters. The GSE parser doesn't make any assumptions about what specific prefix operators or delimiters are allowed, and instead can be told to treat any sequence of characters as a prefix operator or delimiter.
+
+The goal in defining this new format is to provide a unified syntactic framework that can be shared between all S-expression based languages. As such, GSE can provide a foundation for shared tooling between different languages that abstracts away minor details like what kind of prefix operators or delimiters are available in a language. GSE will be able to encapsulate the syntax of most existing Lisps, including Clojure, Scheme, and Common Lisp, but the main goal of GSE is to provide a convenient shared syntactic framework for future languages. Specfically, a primary goal of GSE is to make it easy to define alternative syntaxes for non-lispy languages that can be easily transpiled to and from the original syntax (allowing these languages to gain the benefits of Lisp syntax like macros, structural editing, and, potentially, [visual editing](https://github.com/Ella-Hoeppner/Vlojure)).
+
+## to do
+* describe delimiters and prefixes with a rust type
+  * Something like:
+    * The user defines an enum (or struct, but probably usually an enum), where each value of the enum has a corresponding opening and closing delimiter, tag, and list of enum values for which delimiters are allowed inside.
+    * There is a separate, but similarly defined, enum for prefixes.
+  * I guess the parsing function will need to be generic over the delimiter and prefix type
+    * will it still be possible to have a language that can introduce new delimiters/prefixes dynamically if I go this route, like I had hoped to do in Quoot?
+* write a bunch of tests
+* Parser should operate over a &str or at least a String, rather than a Vec<char>
---
+++ src/parse.rs
@@ -0,0 +1,395 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+  UnmatchedCloser(String),
+  MismatchedCloser(String, String),
+  UnclosedOpener(String),
+  UnclosedString,
+}
+
+fn is_whitespace(c: char) -> bool {
+  c == ' ' || c == ',' || c == '\t' || c == '\n'
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum Sexp {
+  List(Vec<Sexp>),
+  Leaf(String),
+}
+
+impl fmt::Display for Sexp {
+  fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match &self {
+      Sexp::Leaf(token) => fmt.write_str(token)?,
+      Sexp::List(sub_expressions) => {
+        fmt.write_str("(")?;
+        let mut separator = "";
+        for sexp in sub_expressions {
+          fmt.write_str(separator)?;
+          fmt.write_str(&sexp.to_string())?;
+          separator = " ";
+        }
+        fmt.write_str(")")?;
+      }
+    }
+    Ok(())
+  }
+}
+
+pub fn chars_match(pattern: &[char], chars: &[char]) -> bool {
+  chars.len() >= pattern.len()
+    && match (0..pattern.len())
+      .map(|i| pattern[i] == chars[i])
+      .reduce(|acc, e| acc && e)
+    {
+      None => true,
+      Some(b) => b,
+    }
+}
+enum Opening {
+  Prefix,
+  List(&'static [char], &'static [char]),
+}
+struct ParserState {
+  expression_stack: Vec<Vec<Sexp>>,
+  opening_stack: Vec<(usize, Opening)>,
+}
+impl ParserState {
+  pub fn new() -> ParserState {
+    ParserState {
+      expression_stack: vec![vec![]],
+      opening_stack: vec![],
+    }
+  }
+  fn peek_opening(&mut self) -> Option<&Opening> {
+    match self.opening_stack.last() {
+      Some((_, opening)) => Some(&opening),
+      None => None,
+    }
+  }
+  pub fn insert_token(&mut self, token: String) {
+    self.expression_stack[self.opening_stack.len()].push(Sexp::Leaf(token));
+  }
+  pub fn open_prefix(&mut self, char_index: usize, tag: &String) {
+    self.expression_stack.push(vec![]);
+    self.opening_stack.push((char_index, Opening::Prefix));
+    self.insert_token(tag.clone());
+  }
+  pub fn close_expression(&mut self) {
+    let top_expression = self.expression_stack.pop().unwrap();
+    let expression_count = self.expression_stack.len();
+    self.expression_stack[expression_count - 1]
+      .push(Sexp::List(top_expression));
+  }
+  pub fn close_prefixes(&mut self) {
+    while let Some(next_opening) = self.peek_opening() {
+      match next_opening {
+        Opening::Prefix => {
+          self.close_expression();
+          self.opening_stack.pop();
+        }
+        Opening::List(_, _) => break,
+      }
+    }
+  }
+  pub fn open_list(
+    &mut self,
+    char_index: usize,
+    opener: &'static [char],
+    closer: &'static [char],
+    tag: &Option<String>,
+  ) {
+    self.expression_stack.push(vec![]);
+    self
+      .opening_stack
+      .push((char_index, Opening::List(opener, closer)));
+    match tag.clone() {
+      Some(delimiter_tag) => self.insert_token(delimiter_tag),
+      None => (),
+    }
+  }
+  pub fn close_list(&mut self) -> Option<&[char]> {
+    self.close_prefixes();
+    self.close_expression();
+    let closer = match self.opening_stack.pop() {
+      Some(index_opening_pair) => match index_opening_pair.1 {
+        Opening::List(_, closer) => Some(closer),
+        Opening::Prefix => unreachable!(),
+      },
+      None => None,
+    };
+    self.close_prefixes();
+    closer
+  }
+  pub fn get_open_list(&mut self) -> Option<(&[char], &[char])> {
+    self
+      .opening_stack
+      .iter()
+      .rev()
+      .find_map(|index_opening_pair| match index_opening_pair.1 {
+        Opening::Prefix => None,
+        Opening::List(opener, closer) => Some((opener, closer)),
+      })
+  }
+  pub fn finish(&mut self) -> Sexp {
+    self
+      .expression_stack
+      .pop()
+      .unwrap()
+      .first()
+      .unwrap()
+      .clone()
+  }
+}
+
+pub fn parse_chars(chars: Vec<char>) -> Result<Sexp, ParseError> {
+  if chars.is_empty() {
+    return Ok(Sexp::Leaf("nil".to_string()));
+  }
+
+  let delimiters: Vec<(&[char], &[char], Option<String>)> = vec![
+    (&['('], &[')'], None),
+    (&['['], &[']'], Some("#list".to_string())),
+    (&['{'], &['}'], Some("#hashmap".to_string())),
+    (&['#', '{'], &['}'], Some("#hashset".to_string())),
+    (&['#', '['], &[']'], Some("#ordered-hashmap".to_string())),
+  ];
+  let prefixes: Vec<(&[char], String)> = vec![
+    (&['\''], "quote".to_string()),
+    (&['~'], "unquote".to_string()),
+  ];
+
+  let parser_state: &mut ParserState = &mut ParserState::new();
+  let mut expression_opened: bool = false;
+  let mut char_index: usize = 0;
+  let mut consumed_index: usize = 0;
+
+  loop {
+    if char_index >= chars.len() {
+      break;
+    }
+    let char = chars[char_index];
+    let string_opener = char == '"';
+    let prefix_index = if consumed_index == char_index {
+      (0..prefixes.len()).rev().find(|prefix_index| {
+        chars_match(&prefixes[*prefix_index].0, &chars[char_index..])
+      })
+    } else {
+      None
+    };
+    let matched_closer = delimiters
+      .iter()
+      .find(|(_, closer, _)| chars_match(closer, &chars[char_index..]));
+    let open_list = parser_state.get_open_list();
+    let was_expected_closer_matched = match open_list {
+      Some((_, closer)) => chars_match(closer, &chars[char_index..]),
+      None => false,
+    };
+    match matched_closer {
+      None => (),
+      Some(closer) => match open_list {
+        None => {
+          return Err(ParseError::UnmatchedCloser(closer.1.iter().collect()))
+        }
+        Some((opener, _)) => {
+          if !was_expected_closer_matched {
+            return Err(ParseError::MismatchedCloser(
+              opener.iter().collect(),
+              closer.1.iter().collect(),
+            ));
+          }
+        }
+      },
+    }
+    let matched_opening = delimiters
+      .iter()
+      .rev()
+      .find(|delimiter| chars_match(&delimiter.0, &chars[char_index..]));
+    if !was_expected_closer_matched && matched_closer.is_some() {}
+    let whitespace = prefix_index.is_none()
+      && matched_opening.is_none()
+      && !was_expected_closer_matched
+      && is_whitespace(char);
+    if consumed_index != char_index
+      && (string_opener
+        || matched_opening.is_some()
+        || was_expected_closer_matched
+        || whitespace)
+    {
+      // If consumption isn't up to the current index, and this character
+      // indicates the previous token should end, add previous token to AST.
+      parser_state
+        .insert_token(chars[consumed_index..char_index].iter().collect());
+      if whitespace {
+        // If this character is whitespace, indicating that this was the end of
+        // a terminal token rather than a closing delimiter, close any open
+        // prefixes.
+
+        parser_state.close_prefixes();
+      }
+      if !expression_opened {
+        return Ok(parser_state.finish());
+      }
+    };
+    if string_opener {
+      // If this character is a ", indicating the start of a string, consume
+      // the entire string.
+      let string_start_index = char_index;
+      loop {
+        char_index += 1;
+        if char_index >= chars.len() {
+          return Err(ParseError::UnclosedString);
+        }
+        let string_char = chars[char_index];
+        if string_char == '\\' {
+          char_index += 1
+        } else if string_char == '"' {
+          parser_state.insert_token(
+            chars[string_start_index..char_index + 1].iter().collect(),
+          );
+          break;
+        }
+      }
+      if !expression_opened {
+        return Ok(parser_state.finish());
+      }
+    }
+    // Handle the case where this character is the start of a prefix, opener,
+    // or closer, and adjust the character index accordingly.
+    char_index += match prefix_index {
+      Some(i) => {
+        expression_opened = true;
+        // If this is a prefix, add a list, with the tag of the prefix as the
+        // first element of the list, to the AST.
+        let prefix = &prefixes[i];
+        parser_state.open_prefix(char_index, &prefix.1);
+        prefix.0.len()
+      }
+      None => match matched_opening {
+        Some(delimiter) => {
+          expression_opened = true;
+          // If this is an opener, add a list, with the tag of the delimiter
+          // pair (if it has one) as the first element of the list, to the AST.
+          parser_state.open_list(
+            char_index,
+            delimiter.0,
+            delimiter.1,
+            &delimiter.2,
+          );
+          delimiter.0.len()
+        }
+        None => {
+          if was_expected_closer_matched {
+            // This unwrap is safe, because expected_closer_matched will only
+            // be true if there is a list to close.
+            parser_state.close_list().unwrap().len()
+          } else {
+            1
+          }
+        }
+      },
+    };
+    // Catch the consumption up to the current position, if the current
+    // character matched with something for which that should be done.
+    if string_opener
+      || prefix_index.is_some()
+      || matched_opening.is_some()
+      || was_expected_closer_matched
+      || whitespace
+    {
+      consumed_index = char_index;
+    }
+    if expression_opened && parser_state.opening_stack.len() == 0 {
+      return Ok(parser_state.finish());
+    }
+  }
+
+  // If the end of the string was reached and consumption isn't caught up, the
+  // string must end with a token not followed by whitespace, so add one
+  // final leaf to the AST.
+  if char_index >= chars.len() && consumed_index < char_index {
+    parser_state
+      .insert_token(chars[consumed_index..chars.len()].iter().collect());
+  }
+  parser_state.close_prefixes();
+
+  // Throw an error if there are any open lists at the end of the string.
+  match parser_state.get_open_list() {
+    Some((opener, _)) => {
+      return Err(ParseError::UnclosedOpener(opener.iter().collect()))
+    }
+    None => (),
+  }
+  return Ok(parser_state.finish());
+}
+
+pub fn parse(s: &str) -> Result<Sexp, ParseError> {
+  parse_chars(s.chars().collect())
+}
+
+#[test]
+fn test_parse() {
+  [
+    ("()", Sexp::List(vec![])),
+    ("[]", Sexp::List(vec![Sexp::Leaf("list".to_string())])),
+    ("{}", Sexp::List(vec![Sexp::Leaf("hashmap".to_string())])),
+    ("hello!", Sexp::Leaf("hello!".to_string())),
+    (
+      "(+ 1 2)",
+      Sexp::List(vec![
+        Sexp::Leaf("+".to_string()),
+        Sexp::Leaf("1".to_string()),
+        Sexp::Leaf("2".to_string()),
+      ]),
+    ),
+    (
+      "'(+ 1 2)",
+      Sexp::List(vec![
+        Sexp::Leaf("quote".to_string()),
+        Sexp::List(vec![
+          Sexp::Leaf("+".to_string()),
+          Sexp::Leaf("1".to_string()),
+          Sexp::Leaf("2".to_string()),
+        ]),
+      ]),
+    ),
+    (
+      "~'()",
+      Sexp::List(vec![
+        Sexp::Leaf("unquote".to_string()),
+        Sexp::List(vec![Sexp::Leaf("quote".to_string()), Sexp::List(vec![])]),
+      ]),
+    ),
+    (
+      "'a",
+      Sexp::List(vec![
+        Sexp::Leaf("quote".to_string()),
+        Sexp::Leaf("a".to_string()),
+      ]),
+    ),
+    (
+      "''a",
+      Sexp::List(vec![
+        Sexp::Leaf("quote".to_string()),
+        Sexp::List(vec![
+          Sexp::Leaf("quote".to_string()),
+          Sexp::Leaf("a".to_string()),
+        ]),
+      ]),
+    ),
+  ]
+  .into_iter()
+  .for_each(|(str, sexp)| {
+    match parse(str) {
+      Ok(parsed_sexp) => assert_eq!(
+        sexp, parsed_sexp,
+        "String {:?} was not parsed as expected.",
+        str
+      ),
+      Err(e) => {
+        assert!(false, "Failed to parse string {:?}, got error {:?}", str, e)
+      }
+    };
+  });
+}
