731e4c14efa38abba9632f8c008a6b15c0134b83
Ella Hoeppner <ellahoeppner@gmail.com>
2024-03-05 18:03:20 -0500

    reorganizing

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,109 @@ pub enum GSexp<DelimiterType: Delimiter, PrefixType: Prefix> {
   Leaf(String),
 }
 
+fn is_prefix(prefix: &[char], chars: &[char]) -> bool {
+  chars.len() >= prefix.len()
+    && match (0..prefix.len())
+      .map(|i| prefix[i] == chars[i])
+      .reduce(|acc, e| acc && e)
+    {
+      None => true,
+      Some(b) => b,
+    }
+}
+
+fn is_string_prefix(prefix_str: String, chars: &[char]) -> bool {
+  let prefix_chars: Vec<char> = prefix_str.chars().collect();
+  is_prefix(&prefix_chars, chars)
+}
+
+enum PartialGSexp<DelimiterType: Delimiter, PrefixType: Prefix> {
+  Prefixed(PrefixType),
+  Delimited(DelimiterType, Vec<GSexp<DelimiterType, PrefixType>>),
+}
+struct ParserState<DelimiterType: Delimiter, PrefixType: Prefix> {
+  expressions: Vec<GSexp<DelimiterType, PrefixType>>,
+  partial_expression: Vec<PartialGSexp<DelimiterType, PrefixType>>,
+}
+impl<DelimiterType: Delimiter, PrefixType: Prefix>
+  ParserState<DelimiterType, PrefixType>
+{
+  fn new() -> ParserState<DelimiterType, PrefixType> {
+    ParserState {
+      expressions: vec![],
+      partial_expression: vec![],
+    }
+  }
+  fn insert_gsexp(&mut self, mut gsexp: GSexp<DelimiterType, PrefixType>) {
+    loop {
+      if let Some(next_expressions) = self.partial_expression.pop() {
+        match next_expressions {
+          PartialGSexp::Prefixed(prefix_type) => {
+            gsexp = GSexp::Prefixed(prefix_type, Box::new(gsexp))
+          }
+          PartialGSexp::Delimited(delimiter_type, mut sub_expressions) => {
+            sub_expressions.push(gsexp);
+            self
+              .partial_expression
+              .push(PartialGSexp::Delimited(delimiter_type, sub_expressions));
+            break;
+          }
+        }
+      } else {
+        self.expressions.push(gsexp);
+        break;
+      }
+    }
+  }
+  fn insert_leaf(&mut self, token: String) {
+    self.insert_gsexp(GSexp::Leaf(token));
+  }
+  fn open_prefix(&mut self, prefix_type: PrefixType) {
+    self
+      .partial_expression
+      .push(PartialGSexp::Prefixed(prefix_type));
+  }
+  fn open_delimiter(&mut self, delimiter_type: DelimiterType) {
+    self
+      .partial_expression
+      .push(PartialGSexp::Delimited(delimiter_type, vec![]));
+  }
+  fn close_delimiter(&mut self) {
+    match self.partial_expression.pop() {
+      Some(PartialGSexp::Delimited(delimiter_type, sub_expressions)) => {
+        self.insert_gsexp(GSexp::Delimited(delimiter_type, sub_expressions))
+      }
+      Some(PartialGSexp::Prefixed(_)) => panic!(
+        "tried to close delimiter with an open prefix on the top of \
+        partial_expression stack"
+      ),
+      None => {
+        panic!(
+          "tried to close delimiter with nothing on partial_expression stack"
+        )
+      }
+    }
+  }
+  fn get_open_delimiter(&self) -> Option<DelimiterType> {
+    match self.partial_expression.last() {
+      Some(PartialGSexp::Delimited(delimiter_type, _)) => {
+        Some(delimiter_type.clone())
+      }
+      _ => None,
+    }
+  }
+  fn get_open_partial(
+    &self,
+  ) -> Option<&PartialGSexp<DelimiterType, PrefixType>> {
+    self.partial_expression.last()
+  }
+  fn take_last_completed_expression(
+    &mut self,
+  ) -> Option<GSexp<DelimiterType, PrefixType>> {
+    self.expressions.pop()
+  }
+}
+
 impl<DelimiterType: Delimiter, PrefixType: Prefix>
   GSexp<DelimiterType, PrefixType>
 {
@@ -76,293 +179,181 @@ impl<DelimiterType: Delimiter, PrefixType: Prefix>
       },
     }
   }
-}
-
-pub fn is_prefix(prefix: &[char], chars: &[char]) -> bool {
-  chars.len() >= prefix.len()
-    && match (0..prefix.len())
-      .map(|i| prefix[i] == chars[i])
-      .reduce(|acc, e| acc && e)
-    {
-      None => true,
-      Some(b) => b,
+  pub fn parse(
+    delimiters: &[DelimiterType],
+    prefixes: &[PrefixType],
+    s: &str,
+  ) -> Result<Self, ParseError> {
+    let chars: Vec<char> = s.chars().collect();
+    if chars.is_empty() {
+      return Ok(GSexp::Leaf("".to_string()));
     }
-}
 
-pub fn is_string_prefix(prefix_str: String, chars: &[char]) -> bool {
-  let prefix_chars: Vec<char> = prefix_str.chars().collect();
-  is_prefix(&prefix_chars, chars)
-}
+    let parser_state: &mut ParserState<DelimiterType, PrefixType> =
+      &mut ParserState::new();
+    let mut char_index: usize = 0;
+    let mut consumed_index: usize = 0;
 
-enum PartialGSexp<DelimiterType: Delimiter, PrefixType: Prefix> {
-  Prefixed(PrefixType),
-  Delimited(DelimiterType, Vec<GSexp<DelimiterType, PrefixType>>),
-}
-struct ParserState<DelimiterType: Delimiter, PrefixType: Prefix> {
-  expressions: Vec<GSexp<DelimiterType, PrefixType>>,
-  partial_expression: Vec<PartialGSexp<DelimiterType, PrefixType>>,
-}
-impl<DelimiterType: Delimiter, PrefixType: Prefix>
-  ParserState<DelimiterType, PrefixType>
-{
-  pub fn new() -> ParserState<DelimiterType, PrefixType> {
-    ParserState {
-      expressions: vec![],
-      partial_expression: vec![],
-    }
-  }
-  pub fn insert_gsexp(&mut self, mut gsexp: GSexp<DelimiterType, PrefixType>) {
     loop {
-      if let Some(next_expressions) = self.partial_expression.pop() {
-        match next_expressions {
-          PartialGSexp::Prefixed(prefix_type) => {
-            gsexp = GSexp::Prefixed(prefix_type, Box::new(gsexp))
+      if char_index >= chars.len() {
+        break;
+      }
+      let char = chars[char_index];
+      let string_opener = char == '"';
+
+      let prefix_index = if consumed_index == char_index {
+        (0..prefixes.len()).rev().find(|prefix_index| {
+          is_string_prefix(
+            prefixes[*prefix_index].marker(),
+            &chars[char_index..],
+          )
+        })
+      } else {
+        None
+      };
+
+      let matched_closing_delimiter = delimiters.iter().find(|delimiter| {
+        is_string_prefix(delimiter.closer(), &chars[char_index..])
+      });
+      let maybe_open_delimiter = parser_state.get_open_delimiter();
+      let was_expected_closer_matched = match &maybe_open_delimiter {
+        Some(open_delimiter) => {
+          is_string_prefix(open_delimiter.closer(), &chars[char_index..])
+        }
+        None => false,
+      };
+      match &matched_closing_delimiter {
+        None => (),
+        Some(closing_delimiter) => match &maybe_open_delimiter {
+          None => {
+            return Err(ParseError::UnmatchedCloser(closing_delimiter.closer()))
           }
-          PartialGSexp::Delimited(delimiter_type, mut sub_expressions) => {
-            sub_expressions.push(gsexp);
-            self
-              .partial_expression
-              .push(PartialGSexp::Delimited(delimiter_type, sub_expressions));
+          Some(opening_delimiter) => {
+            if !was_expected_closer_matched {
+              return Err(ParseError::MismatchedCloser(
+                opening_delimiter.opener(),
+                closing_delimiter.closer(),
+              ));
+            }
+          }
+        },
+      }
+
+      let matched_opening_delimiter =
+        delimiters.iter().rev().find(|delimiter| {
+          is_string_prefix(delimiter.opener(), &chars[char_index..])
+        });
+      if !was_expected_closer_matched && matched_closing_delimiter.is_some() {}
+      let whitespace = prefix_index.is_none()
+        && matched_opening_delimiter.is_none()
+        && !was_expected_closer_matched
+        && is_whitespace(char);
+      if consumed_index != char_index
+        && (string_opener
+          || matched_opening_delimiter.is_some()
+          || was_expected_closer_matched
+          || whitespace)
+      {
+        // If consumption isn't up to the current index, and this character
+        // indicates the previous token should end, add previous token to AST.
+        parser_state
+          .insert_leaf(chars[consumed_index..char_index].iter().collect());
+        if let Some(completed_expression) =
+          parser_state.take_last_completed_expression()
+        {
+          return Ok(completed_expression);
+        }
+      };
+      if string_opener {
+        // If this character is a ", indicating the start of a string, consume
+        // the entire string.
+        let string_start_index = char_index;
+        loop {
+          char_index += 1;
+          if char_index >= chars.len() {
+            return Err(ParseError::UnclosedString);
+          }
+          let string_char = chars[char_index];
+          if string_char == '\\' {
+            char_index += 1
+          } else if string_char == '"' {
+            parser_state.insert_leaf(
+              chars[string_start_index..char_index + 1].iter().collect(),
+            );
             break;
           }
         }
-      } else {
-        self.expressions.push(gsexp);
-        break;
-      }
-    }
-  }
-  pub fn insert_leaf(&mut self, token: String) {
-    self.insert_gsexp(GSexp::Leaf(token));
-  }
-  pub fn open_prefix(&mut self, prefix_type: PrefixType) {
-    self
-      .partial_expression
-      .push(PartialGSexp::Prefixed(prefix_type));
-  }
-  pub fn open_delimiter(&mut self, delimiter_type: DelimiterType) {
-    self
-      .partial_expression
-      .push(PartialGSexp::Delimited(delimiter_type, vec![]));
-  }
-  pub fn close_delimiter(&mut self) {
-    match self.partial_expression.pop() {
-      Some(PartialGSexp::Delimited(delimiter_type, sub_expressions)) => {
-        self.insert_gsexp(GSexp::Delimited(delimiter_type, sub_expressions))
-      }
-      Some(PartialGSexp::Prefixed(_)) => panic!(
-        "tried to close delimiter with an open prefix on the top of \
-        partial_expression stack"
-      ),
-      None => {
-        panic!(
-          "tried to close delimiter with nothing on partial_expression stack"
-        )
-      }
-    }
-  }
-  pub fn get_open_delimiter(&self) -> Option<DelimiterType> {
-    match self.partial_expression.last() {
-      Some(PartialGSexp::Delimited(delimiter_type, _)) => {
-        Some(delimiter_type.clone())
-      }
-      _ => None,
-    }
-  }
-  pub fn get_open_partial(
-    &self,
-  ) -> Option<&PartialGSexp<DelimiterType, PrefixType>> {
-    self.partial_expression.last()
-  }
-  pub fn take_last_completed_expression(
-    &mut self,
-  ) -> Option<GSexp<DelimiterType, PrefixType>> {
-    self.expressions.pop()
-  }
-}
-
-pub fn parse_chars<DelimiterType: Delimiter, PrefixType: Prefix>(
-  delimiters: &[DelimiterType],
-  prefixes: &[PrefixType],
-  chars: Vec<char>,
-) -> Result<Sexp, ParseError> {
-  if chars.is_empty() {
-    return Ok(Sexp::Leaf("".to_string()));
-  }
-
-  let parser_state: &mut ParserState<DelimiterType, PrefixType> =
-    &mut ParserState::new();
-  let mut char_index: usize = 0;
-  let mut consumed_index: usize = 0;
-
-  loop {
-    if char_index >= chars.len() {
-      break;
-    }
-    let char = chars[char_index];
-    let string_opener = char == '"';
-
-    let prefix_index = if consumed_index == char_index {
-      (0..prefixes.len()).rev().find(|prefix_index| {
-        is_string_prefix(prefixes[*prefix_index].marker(), &chars[char_index..])
-      })
-    } else {
-      None
-    };
-
-    let matched_closing_delimiter = delimiters.iter().find(|delimiter| {
-      is_string_prefix(delimiter.closer(), &chars[char_index..])
-    });
-    let maybe_open_delimiter = parser_state.get_open_delimiter();
-    let was_expected_closer_matched = match &maybe_open_delimiter {
-      Some(open_delimiter) => {
-        is_string_prefix(open_delimiter.closer(), &chars[char_index..])
-      }
-      None => false,
-    };
-    match &matched_closing_delimiter {
-      None => (),
-      Some(closing_delimiter) => match &maybe_open_delimiter {
-        None => {
-          return Err(ParseError::UnmatchedCloser(closing_delimiter.closer()))
+        if let Some(completed_expression) =
+          parser_state.take_last_completed_expression()
+        {
+          return Ok(completed_expression);
         }
-        Some(opening_delimiter) => {
-          if !was_expected_closer_matched {
-            return Err(ParseError::MismatchedCloser(
-              opening_delimiter.opener(),
-              closing_delimiter.closer(),
-            ));
+      }
+      // Handle the case where this character is the start of a prefix, opener,
+      // or closer, and adjust the character index accordingly.
+      char_index += match prefix_index {
+        Some(i) => {
+          // If this is a prefix, add a list, with the tag of the prefix as the
+          // first element of the list, to the AST.
+          parser_state.open_prefix(prefixes[i].clone());
+          prefixes[i].marker().len()
+        }
+        None => match matched_opening_delimiter {
+          Some(delimiter) => {
+            // If this is an opener, open a delimiter.
+            parser_state.open_delimiter(delimiter.clone());
+            delimiter.opener().len()
           }
-        }
-      },
-    }
-
-    let matched_opening_delimiter = delimiters.iter().rev().find(|delimiter| {
-      is_string_prefix(delimiter.opener(), &chars[char_index..])
-    });
-    if !was_expected_closer_matched && matched_closing_delimiter.is_some() {}
-    let whitespace = prefix_index.is_none()
-      && matched_opening_delimiter.is_none()
-      && !was_expected_closer_matched
-      && is_whitespace(char);
-    if consumed_index != char_index
-      && (string_opener
+          None => {
+            if was_expected_closer_matched {
+              // This unwrap is safe, because expected_closer_matched will only
+              // be true if there is a list to close.
+              parser_state.close_delimiter();
+              maybe_open_delimiter.unwrap().closer().len()
+            } else {
+              1
+            }
+          }
+        },
+      };
+      // Catch the consumption up to the current position, if the current
+      // character matched with something for which that should be done.
+      if string_opener
+        || prefix_index.is_some()
         || matched_opening_delimiter.is_some()
         || was_expected_closer_matched
-        || whitespace)
-    {
-      // If consumption isn't up to the current index, and this character
-      // indicates the previous token should end, add previous token to AST.
+        || whitespace
+      {
+        consumed_index = char_index;
+      }
+      if let Some(completed_expression) =
+        parser_state.take_last_completed_expression()
+      {
+        return Ok(completed_expression);
+      }
+    }
+
+    // If the end of the string was reached and consumption isn't caught up, the
+    // string must end with a token not followed by whitespace, so add one
+    // final leaf to the AST.
+    if char_index >= chars.len() && consumed_index < char_index {
       parser_state
-        .insert_leaf(chars[consumed_index..char_index].iter().collect());
-      if let Some(completed_expression) =
-        parser_state.take_last_completed_expression()
-      {
-        return Ok(completed_expression.to_sexp());
-      }
-    };
-    if string_opener {
-      // If this character is a ", indicating the start of a string, consume
-      // the entire string.
-      let string_start_index = char_index;
-      loop {
-        char_index += 1;
-        if char_index >= chars.len() {
-          return Err(ParseError::UnclosedString);
-        }
-        let string_char = chars[char_index];
-        if string_char == '\\' {
-          char_index += 1
-        } else if string_char == '"' {
-          parser_state.insert_leaf(
-            chars[string_start_index..char_index + 1].iter().collect(),
-          );
-          break;
-        }
-      }
-      if let Some(completed_expression) =
-        parser_state.take_last_completed_expression()
-      {
-        return Ok(completed_expression.to_sexp());
-      }
+        .insert_leaf(chars[consumed_index..chars.len()].iter().collect());
     }
-    // Handle the case where this character is the start of a prefix, opener,
-    // or closer, and adjust the character index accordingly.
-    char_index += match prefix_index {
-      Some(i) => {
-        // If this is a prefix, add a list, with the tag of the prefix as the
-        // first element of the list, to the AST.
-        parser_state.open_prefix(prefixes[i].clone());
-        prefixes[i].marker().len()
-      }
-      None => match matched_opening_delimiter {
-        Some(delimiter) => {
-          // If this is an opener, open a delimiter.
-          parser_state.open_delimiter(delimiter.clone());
-          delimiter.opener().len()
+
+    // Throw an error if there are any open lists at the end of the string.
+    if let Some(open_partial) = parser_state.get_open_partial() {
+      return Err(match open_partial {
+        PartialGSexp::Prefixed(prefix) => {
+          ParseError::TrailingPrefix(prefix.marker())
         }
-        None => {
-          if was_expected_closer_matched {
-            // This unwrap is safe, because expected_closer_matched will only
-            // be true if there is a list to close.
-            parser_state.close_delimiter();
-            maybe_open_delimiter.unwrap().closer().len()
-          } else {
-            1
-          }
+        PartialGSexp::Delimited(delimiter, _) => {
+          ParseError::UnclosedOpener(delimiter.opener())
         }
-      },
-    };
-    // Catch the consumption up to the current position, if the current
-    // character matched with something for which that should be done.
-    if string_opener
-      || prefix_index.is_some()
-      || matched_opening_delimiter.is_some()
-      || was_expected_closer_matched
-      || whitespace
-    {
-      consumed_index = char_index;
+      });
     }
-    if let Some(completed_expression) =
-      parser_state.take_last_completed_expression()
-    {
-      return Ok(completed_expression.to_sexp());
-    }
+    Ok(parser_state.take_last_completed_expression().unwrap())
   }
-
-  // If the end of the string was reached and consumption isn't caught up, the
-  // string must end with a token not followed by whitespace, so add one
-  // final leaf to the AST.
-  if char_index >= chars.len() && consumed_index < char_index {
-    parser_state
-      .insert_leaf(chars[consumed_index..chars.len()].iter().collect());
-  }
-
-  // Throw an error if there are any open lists at the end of the string.
-  if let Some(open_partial) = parser_state.get_open_partial() {
-    return Err(match open_partial {
-      PartialGSexp::Prefixed(prefix) => {
-        ParseError::TrailingPrefix(prefix.marker())
-      }
-      PartialGSexp::Delimited(delimiter, _) => {
-        ParseError::UnclosedOpener(delimiter.opener())
-      }
-    });
-  }
-  Ok(
-    parser_state
-      .take_last_completed_expression()
-      .unwrap()
-      .to_sexp(),
-  )
-}
-
-pub fn parse<DelimiterType: Delimiter, PrefixType: Prefix>(
-  delimiters: &[DelimiterType],
-  prefixes: &[PrefixType],
-  s: &str,
-) -> Result<Sexp, ParseError> {
-  parse_chars(delimiters, prefixes, s.chars().collect())
 }
 
 #[allow(dead_code, unused_macros)]
@@ -482,8 +473,10 @@ mod tests {
 
   macro_rules! assert_parse_eq {
     ($string:literal, $sexp:expr) => {
-      let parsed_sexp = parse(&TEST_DELIMITERS, &TEST_PREFIXES, $string)
-        .expect("Failted to parse sexp");
+      let parsed_sexp =
+        TestGSexp::parse(&TEST_DELIMITERS, &TEST_PREFIXES, $string)
+          .expect("Failted to parse string")
+          .to_sexp();
       assert_eq!(
         $sexp, parsed_sexp,
         "String {:?} was not parsed as expected.",
